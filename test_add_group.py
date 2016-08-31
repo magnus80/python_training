c8790611370f3d82afa21c23de64e2ca70734750
magnus80 <zarechny58@gmail.com>
2016-08-31 12:28:13 +0300

    Перенесены некоторые методы

--- test_add_group.py
+++ test_add_group.py
@@ -5,14 +5,6 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from group import Group
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-
 class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
@@ -20,36 +12,27 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.init_group_creation(wd)
         self.create_group(wd, Group(name="dfdsfsdf", header="sdfsdfsdf", footer="sdfsdfsf"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.init_group_creation(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
-
 
     def logout(self, wd):
         # logout
         wd.find_element_by_link_text("Logout").click()
 
-
     def return_to_groups_page(self, wd):
         #  return to groups page
         wd.find_element_by_link_text("group page").click()
 
-
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
+        self.init_group_creation(wd)
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -62,19 +45,18 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-
+        self.return_to_groups_page(wd)
 
     def init_group_creation(self, wd):
         # init group creation
         wd.find_element_by_name("new").click()
 
-
     def open_groups_page(self, wd):
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
-
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -84,10 +66,8 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
-
         # open home page
         wd.get("http://localhost:81/addressbook/")
-
 
     def tearDown(self):
         self.wd.quit()
